2447e6bcc314c3e8df1f9f979131ca627764eefb
Hproko <henriqueprokopenko@hotmail.com>
2022-04-21 17:50:15 -0300

    atualizando script

--- script.sh
+++ script.sh
@@ -12,11 +12,40 @@ then
 	mkdir outputs
 fi
 
-for N in 10 32 #50 64 100 128 200 250 256 300 400 512 600 1000 1024 2000 2048 3000 4096
+for N in 10 32 50 64 100 128 200 250 256 300 400 512 600 1000 1024 2000 2048 3000 4096
 do
 	echo "Calculando rosenbrock $N variaveis"
 
 	L3=$(likwid-perfctr -C 3 -g L3 -m ./newtonPC < entradas_rosenbrock/rosenbrock$N.in -o outputs/out$N.dat | grep "L3 bandwidth" | cut -d'|' -f3)
 	
-	echo $L3
+	echo "$N $(echo $L3 | cut -d' ' -f1)" >> tabelas/newtonPadraoL3.dat
+
+	echo "$N $(echo $L3 | cut -d' ' -f2)" >> tabelas/MatrizHessNPL3.dat
+
+	echo "$N $(echo $L3 | cut -d' ' -f3)" >> tabelas/VetGradNPL3.dat
+
+	echo "$N $(echo $L3 | cut -d' ' -f4)" >> tabelas/SistLinearNPL3.dat
+	
+	echo "$N $(echo $L3 | cut -d' ' -f5)" >> tabelas/newtonInexatoL3.dat
+	
+	echo "$N $(echo $L3 | cut -d' ' -f6)" >> tabelas/VetGradNIL3.dat
+	
+	echo "$N $(echo $L3 | cut -d' ' -f7)" >> tabelas/MatrizHessNIL3.dat
+	
+	echo "$N $(echo $L3 | cut -d' ' -f8)" >> tabelas/SistLinearNIL3.dat
+
+	L2CACHE=$(likwid-perfctr -C 3 -g L2CACHE -m ./newtonPC < entradas_rosenbrock/rosenbrock$N.in -o outputs/out$N.dat | grep "L2 miss ratio" | cut -d'|' -f3)
+
+
+	TIME=$(likwid-perfctr -C 3 -g L3 -m ./newtonPC < entradas_rosenbrock/rosenbrock$N.in -o outputs/out$N.dat| grep "RDTSC Runtime" | cut -d'|' -f3)
+
+	
+
+	likwid-perfctr -C 3 -g FLOPS_DP -m -o out.txt ./newtonPC < entradas_rosenbrock/rosenbrock$N.in -o outputs/out$N.dat
+
+	FLOPS=$(cat out.txt | grep "MFLOP/s" | cut -d'|' -f3)
+	
+	rm out.txt
+
+
 done
